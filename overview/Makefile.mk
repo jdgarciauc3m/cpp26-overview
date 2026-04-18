9a907ca90e9f674ab3c5db9cd781ce32934be82b
J. Daniel Garcia <jdgarcia@inf.uc3m.es>
2026-04-18 13:08:00 +0200

    Initial structure

--- overview/Makefile.mk
+++ overview/Makefile.mk
@@ -1,4 +1,8 @@
-#include $(OVERVIEW_STL_DIR)/Makefile.mk
+include $(OVERVIEW_REFLECTION_DIR)/Makefile.mk
+include $(OVERVIEW_UBSAFETY_DIR)/Makefile.mk
+include $(OVERVIEW_CONTRACTS_DIR)/Makefile.mk
+include $(OVERVIEW_SENDRECV_DIR)/Makefile.mk
+include $(OVERVIEW_PARALLEL_DIR)/Makefile.mk
 
 $(FINAL_OVERVIEW_PDF): $(OVERVIEW_PARTS) $(CONFIG_PARTS) | $(FINAL_DIR)
 	$(LATEXMK) -jobname=$(subst /,-,$(<D)) $(OVERVIEW_DIR)/slides
